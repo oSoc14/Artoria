f11efe370d81159b56e5617776a8b237153dc84c
dieterbeelaert <dieterbeelaert3@msn.com>
2014-07-01 11:15:12 +0200

    mainactivity ...

---
+++ app/src/main/java/be/artoria/belfortapp/activities/MainActivity.java
@@ -0,0 +1,83 @@
+package be.artoria.belfortapp.activities;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import be.artoria.belfortapp.R;
+
+public class MainActivity extends BaseActivity {
+    ArrayAdapter<String> menuAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initGui();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*initialize the GUI content and clickhandlers*/
+    private void initGui(){
+        ListView lstMenu = (ListView)findViewById(R.id.lstMenu);
+        Button btnSettings = (Button)findViewById(R.id.btnSettings);
+        Button btnAbout = (Button)findViewById(R.id.btnAbout);
+
+
+        menuAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.lstMenu));
+        lstMenu.setAdapter(menuAdapter);
+
+        lstMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Go to settings*/
+                Intent i = new Intent(MainActivity.this,LanguageChoiceActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Go to the Artoria website*/
+                Uri webpage = Uri.parse(getResources().getString(R.string.artoria_url));
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+            }
+        });
+    }
+}
