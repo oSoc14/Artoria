1679890ece87354c8d4d0d5e8bc441f4a7b930a9
lbdgraev <laurens.degraeve@ugent.be>
2014-07-01 13:30:09 +0200

    added waypoint class + new fragment

---
+++ app/src/main/java/be/artoria/belfortapp/app/Waypoint.java
@@ -0,0 +1,14 @@
+package be.artoria.belfortapp.app;
+
+/**
+ * Created by Laurens on 01/07/2014.
+ */
+public class Waypoint {
+
+    public double lat;
+    public int id;
+    public double lon;
+    public String name;
+    public String description;
+
+}
