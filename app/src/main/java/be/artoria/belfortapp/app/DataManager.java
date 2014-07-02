e4441fa75378ac4057622ec753e7e6b6133613bb
dieterbeelaert <dieterbeelaert3@msn.com>
2014-07-02 18:50:05 +0200

    #9 added toast message when building is added to route + possibility to go from route to building view (in progress)

--- app/src/main/java/be/artoria/belfortapp/app/DataManager.java
+++ app/src/main/java/be/artoria/belfortapp/app/DataManager.java
@@ -14,18 +14,21 @@ public class DataManager {
     private static final Language lang = null;
 
     private static final DataManager INSTANCE = new DataManager();
-    public static final List<POI> poiList = new ArrayList<POI>();
+    public static List<POI> poiList = new ArrayList<POI>();
 
     /*Route start point (belfort Ghent) needs to be replaced by current location in the future*/
     public static final double BELFORT_LON = 3.724833;
     public static final double BELFORT_LAT = 51.053623;
 
 
-    // Private constructor prevents instantiation from other classes
-    private DataManager() {
+    static{
         poiList.add(new POI(0,"51.053939", "3.722958","Sint-Niklaaskerk","SINT_NIKLAAS KERK WAUW","http://www.sintniklaaskerk.be/media/photos/560/021f552775075dd9e053e30072ea21045d628ef3.jpg"));
         poiList.add(new POI(1,"51.053952", "3.722196","Korenmarkt","YEEEEEEE","http://fideel.files.wordpress.com/2009/08/korenmarkt-ca-1910-2.jpg"));
         poiList.add(new POI(2,"51.054562", "3.724862","Stadhuis","WOOOHOOOOOOOOO HOOOO HOOOO","http://upload.wikimedia.org/wikipedia/commons/2/2c/Gent_stadhuis_belfort_1890-1900.jpg"));
+    }
+    // Private constructor prevents instantiation from other classes
+    private DataManager() {
+
     }
 
     public Language getCurrentLanguage(){
