5ac908e00811a18fb97a427cb31c090cd867fba5
dieterbeelaert <dieterbeelaert3@msn.com>
2014-07-03 17:42:46 +0200

    bugfix

--- app/src/main/java/be/artoria/belfortapp/app/DataManager.java
+++ app/src/main/java/be/artoria/belfortapp/app/DataManager.java
@@ -1,6 +1,6 @@
 package be.artoria.belfortapp.app;
 
-import android.content.res.Resources
+import android.content.res.Resources;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,15 +31,12 @@ public class DataManager {
     // Private constructor prevents instantiation from other classes
     private DataManager() {    }
 
-    static{
-       // poiList.add(new POI(0,"51.053939", "3.722958","Sint-Niklaaskerk","SINT_NIKLAAS KERK WAUW","http://www.sintniklaaskerk.be/media/photos/560/021f552775075dd9e053e30072ea21045d628ef3.jpg"));
-       // poiList.add(new POI(1,"51.053952", "3.722196","Korenmarkt","YEEEEEEE","http://fideel.files.wordpress.com/2009/08/korenmarkt-ca-1910-2.jpg"));
-       // poiList.add(new POI(2,"51.054562", "3.724862","Stadhuis","WOOOHOOOOOOOOO HOOOO HOOOO","http://upload.wikimedia.org/wikipedia/commons/2/2c/Gent_stadhuis_belfort_1890-1900.jpg"));
-    static{
+    static {
         //poiList.add(new POI(0,"51.053939", "3.722958","Sint-Niklaaskerk","SINT_NIKLAAS KERK WAUW","http://www.sintniklaaskerk.be/media/photos/560/021f552775075dd9e053e30072ea21045d628ef3.jpg"));
         //poiList.add(new POI(1,"51.053952", "3.722196","Korenmarkt","YEEEEEEE","http://fideel.files.wordpress.com/2009/08/korenmarkt-ca-1910-2.jpg"));
         //poiList.add(new POI(2,"51.054562", "3.724862","Stadhuis","WOOOHOOOOOOOOO HOOOO HOOOO","http://upload.wikimedia.org/wikipedia/commons/2/2c/Gent_stadhuis_belfort_1890-1900.jpg"));
-    public Language getCurrentLanguage(){
+    }
+        public Language getCurrentLanguage(){
         if(lang == null) lang = PrefUtils.getLanguage();
         return lang;
     }
