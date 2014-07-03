0a773a7f8e75a510e70afa79ab7dd930caf54ad3
laurens-dg <laurens.degraeve@outlook.com>
2014-07-03 14:10:22 +0200

    trying to make the list dragsortable #16

--- app/src/main/java/be/artoria/belfortapp/activities/RouteActivity.java
+++ app/src/main/java/be/artoria/belfortapp/activities/RouteActivity.java
@@ -10,10 +10,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.mobeta.android.dslv.DragSortController;
 import com.mobeta.android.dslv.DragSortListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import be.artoria.belfortapp.R;
-import be.artoria.belfortapp.app.DataManager;
 import be.artoria.belfortapp.app.POI;
 import be.artoria.belfortapp.app.RouteManager;
 
@@ -21,7 +24,7 @@ import be.artoria.belfortapp.app.RouteManager;
 public class RouteActivity extends BaseActivity {
 
     DragSortListView listView;
-    ArrayAdapter<String> adapter;
+    ArrayAdapter<POI> adapter;
 
     private DragSortListView.DropListener onDrop = new DragSortListView.DropListener()
     {
@@ -30,7 +33,7 @@ public class RouteActivity extends BaseActivity {
         {
             if (from != to)
             {
-                String item = adapter.getItem(from);
+                POI item = adapter.getItem(from);
                 adapter.remove(item);
                 adapter.insert(item, to);
             }
@@ -51,28 +54,8 @@ public class RouteActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        listView = (DragSortListView) findViewById(R.id.listview);
-        String[] names = getResources().getStringArray(R.array.random_names);
-        ArrayList<String> list = new ArrayList<String>(Arrays.asList(names));
-        adapter = new ArrayAdapter<String>(this,
-                R.layout.item_layout, R.id.textView1, list);
-        listView.setAdapter(adapter);
-        listView.setDropListener(onDrop);
-        listView.setRemoveListener(onRemove);
-
-        DragSortController controller = new DragSortController(listView);
-        controller.setDragHandleId(R.id.imageView1);
-        //controller.setClickRemoveId(R.id.);
-        controller.setRemoveEnabled(false);
-        controller.setSortEnabled(true);
-        controller.setDragInitMode(1);
-        //controller.setRemoveMode(removeMode);
-
-        listView.setFloatViewManager(controller);
-        listView.setOnTouchListener(controller);
-        listView.setDragEnabled(true);
+        setContentView(R.layout.activity_route);
+        initGUI();
     }
 
     @Override
@@ -86,20 +69,6 @@ public class RouteActivity extends BaseActivity {
 
     private ArrayAdapter<POI> routeAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_route);
-        initGUI();
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.route, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -115,32 +84,50 @@ public class RouteActivity extends BaseActivity {
 
     /*Initialize GUI */
     private void initGUI(){
-        final ListView lstRoute = (ListView)findViewById(R.id.lstRoute);
-        final Button btnCalcRoute = (Button)findViewById(R.id.btnCalcRoute);
+        //final ListView lstRoute = (ListView)findViewById(R.id.lstRoute);
+        //final Button btnCalcRoute = (Button)findViewById(R.id.btnCalcRoute);
 
 
-        /*TODO make the list sortable, this might be interesting: http://jasonmcreynolds.com/?p=423 */
-        routeAdapter = new ArrayAdapter<POI>(this,android.R.layout.simple_list_item_1, RouteManager.getInstance().getWaypoints());
-        lstRoute.setAdapter(routeAdapter);
+        ///*TODO make the list sortable, this might be interesting: http://jasonmcreynolds.com/?p=423 */
+        //routeAdapter = new ArrayAdapter<POI>(this,android.R.layout.simple_list_item_1, RouteManager.getInstance().getWaypoints());
+        //lstRoute.setAdapter(routeAdapter);
 
-        btnCalcRoute.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final Intent i = new Intent(RouteActivity.this,MapActivity.class);
-                startActivity(i);
-            }
-        });
+        /* new code */
 
-        lstRoute.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView = (DragSortListView) findViewById(R.id.lstRoute);
+        adapter = new ArrayAdapter<POI>(this,
+                android.R.layout.simple_list_item_1,  RouteManager.getInstance().getWaypoints());
+        listView.setAdapter(adapter);
+        listView.setDropListener(onDrop);
+        listView.setRemoveListener(onRemove);
+
+        DragSortController controller = new DragSortController(listView);
+        controller.setDragHandleId(R.id.drag_handler);
+        //controller.setClickRemoveId(R.id.);
+        controller.setRemoveEnabled(false);
+        controller.setSortEnabled(true);
+        controller.setDragInitMode(1);
+        //controller.setRemoveMode(removeMode);
+
+        listView.setFloatViewManager(controller);
+        listView.setOnTouchListener(controller);
+        listView.setDragEnabled(true);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 //TODO: extract id from selected item (reverse lookup?)
-                final POI poi = (POI)lstRoute.getSelectedItem();
+                final POI poi = (POI)listView.getSelectedItem();
                 final int id = poi.id;
                 final Intent intent = new Intent(RouteActivity.this, MonumentDetailActivity.class);
                 intent.putExtra("id", id);
                 startActivity(intent);
             }
         });
+    }
+
+    public void goToCalcRoute(View view) {
+        final Intent i = new Intent(RouteActivity.this,MapActivity.class);
+        startActivity(i);
     }
 }
