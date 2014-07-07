733e639a91cbb6a695016ee1dafefb60efdb6b3a
dieterbeelaert <dieterbeelaert3@msn.com>
2014-07-07 10:09:41 +0200

    #33 fixed issue by adding a check ...

--- app/src/main/java/be/artoria/belfortapp/activities/MapActivity.java
+++ app/src/main/java/be/artoria/belfortapp/activities/MapActivity.java
@@ -59,6 +59,7 @@ public class MapActivity extends BaseActivity {
     public static final String LANG_NL = "nl_NL";
     private MapView mapView;
     private boolean showsMap = true;
+    private boolean firstStart = true; //to check when the mapview is loaded for the first time (this is for the ViewTreeObserver)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -109,7 +110,11 @@ public class MapActivity extends BaseActivity {
         vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                mapView.getController().setCenter(getCurrentLocation());
+                if(firstStart){
+                    mapView.getController().setCenter(getCurrentLocation());
+                    firstStart = false;
+                }
+
             }
         });
 
