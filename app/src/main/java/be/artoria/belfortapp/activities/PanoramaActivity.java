6e59a70f2b829db1afacd8e0647f14c5d78c7cdd
laurens-dg <laurens.degraeve@outlook.com>
2015-01-20 16:33:50 +0100

    Fixed issue with appcompat

--- app/src/main/java/be/artoria/belfortapp/activities/PanoramaActivity.java
+++ app/src/main/java/be/artoria/belfortapp/activities/PanoramaActivity.java
@@ -1,6 +1,8 @@
 package be.artoria.belfortapp.activities;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
@@ -14,6 +16,12 @@ public class PanoramaActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_panorama);
         initGui();
     }
